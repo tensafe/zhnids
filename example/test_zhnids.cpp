cafd7f88f11556d4e1bc24b0a8087e27f65cdc8a
tensafe <as491621734@gmail.com>
2014-12-25 17:34:38 +0800

    aaa
    aaa

--- example/test_zhnids.cpp
+++ example/test_zhnids.cpp
@@ -1,46 +1,94 @@
 #include <iostream>
-#include <zhnids.hpp>
+//#include <zhnids.hpp>
 #include <vector>
 #include <list>
+#include <string>
 
-#include <boost/timer.hpp>
+#include <boost/locale.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/typeof/typeof.hpp>
+
+#include <map>
 
 using namespace std;
 
-typedef list<unsigned char> vector_data;
+//#include <boost/timer.hpp>
+//
+//using namespace std;
+//
+//typedef list<unsigned char> vector_data;
+//
+//vector_data vector_data_;
+//
+//
+//bool tcp_rehandler(xzh::tcp_packet_node_ptr l_tcp_queue_node)
+//{
+//	
+//
+//	if (l_tcp_queue_node)
+//	{
+//		//cout << l_tcp_queue_node->gets_ip() << endl;
+//		boost::timer timer_;
+//		cout << "start..." << endl;
+//		std::copy(l_tcp_queue_node->get_tcp_packet_data().begin(), l_tcp_queue_node->get_tcp_packet_data().end(), vector_data_.end());
+//		cout << "endl..." <<timer_.elapsed()<<endl;
+//	}
+//	
+//	return true;
+//}
+//
+//bool udp_rehandler(xzh::udp_packet_node_ptr l_udp_packet)
+//{
+//	if (l_udp_packet)
+//	{
+//		cout << "udp ..." << l_udp_packet->getdatalen() << endl;
+//	}
+//	
+//	return true;
+//}
 
-vector_data vector_data_;
-
-
-bool tcp_rehandler(xzh::tcp_packet_node_ptr l_tcp_queue_node)
+bool url_decode(const std::string& in, std::string& out)
 {
-	
-
-	if (l_tcp_queue_node)
+	out.clear();
+	out.reserve(in.size());
+	for (std::size_t i = 0; i < in.size(); ++i)
 	{
-		//cout << l_tcp_queue_node->gets_ip() << endl;
-		boost::timer timer_;
-		cout << "start..." << endl;
-		std::copy(l_tcp_queue_node->get_tcp_packet_data().begin(), l_tcp_queue_node->get_tcp_packet_data().end(), vector_data_.end());
-		cout << "endl..." <<timer_.elapsed()<<endl;
+		if (in[i] == '%')
+		{
+			if (i + 3 <= in.size())
+			{
+				int value = 0;
+				std::istringstream is(in.substr(i + 1, 2));
+				if (is >> std::hex >> value)
+				{
+					out += static_cast<char>(value);
+					i += 2;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if (in[i] == '+')
+		{
+			out += ' ';
+		}
+		else
+		{
+			out += in[i];
+		}
 	}
-	
-	return true;
-}
-
-bool udp_rehandler(xzh::udp_packet_node_ptr l_udp_packet)
-{
-	if (l_udp_packet)
-	{
-		cout << "udp ..." << l_udp_packet->getdatalen() << endl;
-	}
-	
 	return true;
 }
 
 void main()
 {
-	xzh::tcp_repacket l_tcp_repacket;
+	/*xzh::tcp_repacket l_tcp_repacket;
 	l_tcp_repacket.add_repacket_handler("tcp_repacket", tcp_rehandler);
 
 	xzh::udppacket l_udp_packet;
@@ -58,7 +106,31 @@ void main()
 	
 	xzh::xzhnids l_test_nids;
 	l_test_nids.add_ipfrag_handler("ip", boost::bind(&xzh::ipfragment::ipfrag_handler, &l_ipfragment, _1, _2, _3));
-	l_test_nids.start("tcp port 80", 100, 10);
+	l_test_nids.start("tcp port 80", 100, 10);*/
+	std::string strurl = "http://set3.mail.qq.com/cgi-bin/ftnCreatefile?uin=&ef=js&resp_charset=UTF8&s=ftnCreate&sid=dRN5EJ39tPliBxpO&dirid=&path=C%3A%5CUsers%5CAdministrator%5CDesktop%5CTCP-IP%E6%8A%80%E6%9C%AF%E5%A4%A7%E5%85%A8%5C037.PDF&size=446546&md5=4dd15b76d4f3b442de1782da0cefe981&sha=65b33bfcc271ccdd15663183187ea2e152d2cbec&sha3=&appid=2&loc=ftnCreatefile,ftnCreatefile,ftnCreate,attach2";
+
+	std::string strdurl;
+	url_decode(strurl, strdurl);
+
+	std::string stre = boost::locale::conv::from_utf(strdurl, "GBK");
+
+	typedef map<string, string> string_map;
+	string_map map_;
+
+	vector<boost::iterator_range<string::iterator> > l;
+	boost::algorithm::split(l, stre, boost::algorithm::is_any_of("&"));
+
+	for (BOOST_AUTO(pos, l.begin()); pos != l.end(); pos++)
+	{
+		vector<boost::iterator_range<string::iterator> > l_k;
+		boost::algorithm::split(l_k, *pos, boost::algorithm::is_any_of("="));
+		for (BOOST_AUTO(pos_k, l_k.begin()); pos_k != l_k.end(); advance(pos_k, 2))
+		{
+			string strk = boost::copy_range<std::string>(*pos_k);
+			string strv = boost::copy_range<std::string>(*(pos_k + 1));
+			map_[strk] = strv;
+		}
+	}
 
 	getchar();
 }
